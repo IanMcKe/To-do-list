51a32c10c9e087f8f988037073297633315643ee
Ian McKenney & Julio Diaz <student@epicodus.com>
2015-08-05 11:43:51 -0700

    fixed twig naming

--- app/app.php
+++ app/app.php
@@ -1,4 +1,4 @@
-<?php
+html.<?php
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Task.php";
 
@@ -13,16 +13,16 @@
     ));
 
     $app->get("/", function() use ($app) {
-        return $app['twig']->render('tasks.twig', array('tasks' => Task::getAll()));
+        return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
     });
     $app->post("/tasks", function() use ($app) {
         $task = new Task($_POST['description']);
         $task->save();
-        return $app['twig']->render('create_task.twig', array('newtask' => $task));
+        return $app['twig']->render('create_task.html.twig', array('newtask' => $task));
     });
     $app->post("/delete_tasks", function()  use ($app){
             Task::deleteAll();
-            return $app['twig']->render('delete_tasks.twig');
+            return $app['twig']->render('delete_tasks.html.twig');
     });
 
     return $app;
